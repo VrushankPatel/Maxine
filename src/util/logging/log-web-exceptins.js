c7e1e23acba1a6589f3cb82d6a8439c0bcf4e66b
Vrushank Patel <vrushankpatel5@gmail.com>
2022-02-25 18:56:58 +0530

    reverted to i-else

--- src/util/logging/log-web-exceptins.js
+++ src/util/logging/log-web-exceptins.js
@@ -2,17 +2,13 @@ const { httpStatus } = require("../constants/constants");
 const { logExceptions } = require("./logging-util");
 
 const logWebExceptions = (err, req, res, next) => {
-    switch(err.code){
-        case 'ENOENT':
+    if(err.code === 'ENOENT'){
         logExceptions(req, httpStatus.MSG_FILE_NOT_FOUND);
         res.status(httpStatus.STATUS_NOT_FOUND).json({"message" : httpStatus.MSG_FILE_NOT_FOUND});
-        break;
-
-        default:
-        logExceptions(req, err.toString());
-        res.status(httpStatus.STATUS_SERVER_ERROR).json({"message" : httpStatus.MSG_MAXINE_SERVER_ERROR});
-        break;
+        return;
     }
+    logExceptions(req, err.toString());
+    res.status(httpStatus.STATUS_SERVER_ERROR).json({"message" : httpStatus.MSG_MAXINE_SERVER_ERROR});
 }
 
 module.exports = logWebExceptions;
