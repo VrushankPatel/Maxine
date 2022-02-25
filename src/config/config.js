c1408ef9ee13be8e9659c5c4ed3bf009a9807ae8
Vrushank Patel <vrushankpatel5@gmail.com>
2022-02-25 18:24:38 +0530

    kept index.html only

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-const { actuatorConfig, statusMonitorConfig } = require("./configs/actuator-config");
-const { logFileTransports } = require("./configs/logging-config");
-
-module.exports = {
-    actuatorConfig,
-    statusMonitorConfig,
-    logFileTransports
-};
