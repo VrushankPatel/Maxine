15415d525855d3357c42f128fa61a7531fccbdd4
Vrushank Patel <vrushankpatel5@gmail.com>
2022-02-25 18:14:42 +0530

    used switch case

--- src/util/logging/log-web-exceptins.js
+++ src/util/logging/log-web-exceptins.js
@@ -2,13 +2,17 @@ const { httpStatus } = require("../constants/constants");
 const { logExceptions } = require("./logging-util");
 
 const logWebExceptions = (err, req, res, next) => {
-    if(err.code === 'ENOENT'){
+    switch(err.code){
+        case 'ENOENT':
         logExceptions(req, httpStatus.MSG_FILE_NOT_FOUND);
         res.status(httpStatus.STATUS_NOT_FOUND).json({"message" : httpStatus.MSG_FILE_NOT_FOUND});
-        return;
+        break;
+
+        default:
+        logExceptions(req, err.toString());
+        res.status(httpStatus.STATUS_SERVER_ERROR).json({"message" : httpStatus.MSG_MAXINE_SERVER_ERROR});
+        break;
     }
-    logExceptions(req, err.toString());
-    res.status(httpStatus.STATUS_SERVER_ERROR).json({"message" : httpStatus.MSG_MAXINE_SERVER_ERROR});
 }
 
 module.exports = logWebExceptions;
