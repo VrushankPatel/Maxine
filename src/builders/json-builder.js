9f50e30678f1958f6e591cc6eb215b833db2a3ab
Vrushank Patel <vrushankpatel5@gmail.com>
2022-02-28 16:04:26 +0530

    added json builder test and corrected some mistakes.

--- src/builders/json-builder.js
+++ src/builders/json-builder.js
@@ -1,7 +1,7 @@
 const { properties } = require('../util/propertyReader/property-reader');
 class JsonBuilder{
     jsonObj;
-    checkCondition;
+    doCheckCondition;
     registeredObj;
     conditionStack = [];
 
@@ -13,28 +13,32 @@ class JsonBuilder{
 
     static loadJson = (jsonObj) => new JsonBuilder(jsonObj);
 
-    checkNull = (element) => {
-        this.conditionStack.push(element !== null);
-        this.checkCondition = true;
+    checkCondition = (condition) => {
+        this.conditionStack.push(condition);
+        this.doCheckCondition = true;
         return this;
+    }
+
+    checkNull = (element) => {
+        return this.checkCondition(element !== null);
     }
 
     endCondition = () => {
         this.conditionStack.pop();
         if(this.conditionStack.length === 0){
-            this.checkCondition = null;
+            this.doCheckCondition = null;
         }
         return this;
     }
 
     endAllConditions = () => {
         this.conditionStack = [];
-        this.checkCondition = null;
+        this.doCheckCondition = null;
         return this
     }
 
     put = (key, value) => {
-        if(this.checkCondition){
+        if(this.doCheckCondition){
             if(this.conditionStack.every(e => e === true)){
                 this.jsonObj[key] = value;
             }
@@ -49,8 +53,8 @@ class JsonBuilder{
         return this;
     }
 
-    putFromRegObj = (key, refObj) => {
-        if(this.checkCondition){
+    putFromRegObj = (refObj, key = refObj) => {
+        if(this.doCheckCondition){
             if(this.conditionStack.slice(-1)[0]){
                 this.jsonObj[key] = this.registeredObj[refObj];
             }
---
+++ test/json-builder.test.js
@@ -0,0 +1,71 @@
+const JsonBuilder = require("../src/builders/json-builder");
+
+const fileName = require('path').basename(__filename).replace(".js","");
+
+describe(`${fileName} : JSON Builder Test`, () => {
+
+    it('Building JSON Object by JSONBuilder and checking if it works with all the conditions', (done) => {
+        let jsonSample = JsonBuilder.createNewJson().getJson();
+        jsonSample.should.be.a('object');
+
+        jsonSample = JsonBuilder.loadJson(jsonSample)
+                                .put("sampleKey", "sampleValue") // should store
+                                .checkCondition(true)
+                                    .put("sampleKeyShouldBeHere", "sampleValueShouldBeHere") // should store
+                                .endCondition()
+                                .checkCondition(true)
+                                    .checkCondition(true)
+                                        .checkCondition(false)
+                                            .put("sampleKeyShouldNotBeHere", "sampleValueShouldNotBeHere") // shouldn't store
+                                .endAllConditions()
+                                .checkCondition(true)
+                                    .checkCondition(false)
+                                        .put("keyUnderFalseCondition","valueUnderFalseCondition") // shouldn't store
+                                    .endCondition()
+                                    .checkCondition(true)
+                                        .put("keyUnderTrueCondition","valueUnderTrueCondition") // should store
+                                    .endCondition()
+                                    .put("keyUnderTrueConditionAgain","valueUnderTrueConditionAgain") // should store
+                                .endAllConditions()
+                                .getJson();
+
+        jsonSample.should.have.own.property("sampleKey", "sampleValue");
+
+        jsonSample.should.have.own.property("sampleKeyShouldBeHere", "sampleValueShouldBeHere");
+
+        jsonSample.should.not.have.own.property("sampleKeyShouldNotBeHere", "sampleValueShouldNotBeHere");
+        jsonSample.should.not.have.own.property("keyUnderFalseCondition", "valueUnderFalseCondition");
+        jsonSample.should.have.own.property("keyUnderTrueCondition", "valueUnderTrueCondition");
+        jsonSample.should.have.own.property("keyUnderTrueConditionAgain", "valueUnderTrueConditionAgain");
+        done();
+    });
+
+    it('Building JSON Object by JSONBuilder\'s reference from another Object.', (done) => {
+        const sampleObject = {"key1" : "value1","key2" : "value2","key3" : "value3"}
+
+        const sampleObject2 = {"sampleKey1" : "sampleValue1","sampleKey2" : "sampleValue2","sampleKey3" : "sampleValue3"};
+
+        let jsonSample = JsonBuilder.createNewJson()
+                                    .registerObj(sampleObject) // same key as reference object (key1 -> key1)
+                                        .putFromRegObj("key1")
+                                        .putFromRegObj("key2")
+                                        .putFromRegObj("key3")
+                                    .deregisterObj()
+                                    .registerObj(sampleObject2) // different key name for target obejct (sampleKey1 -> custSampleKey1)
+                                        .putFromRegObj("sampleKey1", "custSampleKey1")
+                                        .putFromRegObj("sampleKey2", "custSampleKey2")
+                                        .putFromRegObj("sampleKey3", "custSampleKey3")
+                                    .deregisterObj()
+                                    .getJson();
+
+        jsonSample.should.have.own.property("key1", "value1");
+        jsonSample.should.have.own.property("key2", "value2");
+        jsonSample.should.have.own.property("key3", "value3");
+
+        jsonSample.should.have.own.property("custSampleKey1", "sampleValue1");
+        jsonSample.should.have.own.property("custSampleKey2", "sampleValue2");
+        jsonSample.should.have.own.property("custSampleKey3", "sampleValue3");
+
+        done()
+    });
+});
