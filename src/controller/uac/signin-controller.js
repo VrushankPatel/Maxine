5a6594cef6174ad1bfee8116c777272a49ba3394
Vrushank Patel <vrushankpatel5@gmail.com>
2022-04-07 15:14:49 +0530

    signin user check enabled

--- src/controller/uac/signin-controller.js
+++ src/controller/uac/signin-controller.js
@@ -1,6 +1,8 @@
+const { User, admin } = require("../../entity/user");
 const { generateAccessToken, authenticateToken } = require("../../security/jwt");
 const { statusAndMsgs } = require("../../util/constants/constants");
 const { error } = require("../../util/logging/logging-util");
+const _ = require('lodash');
 
 const signInController = (req, res) => {
     const {userName, password} = req.body;
@@ -9,8 +11,12 @@ const signInController = (req, res) => {
         res.status(statusAndMsgs.STATUS_GENERIC_ERROR).json({"message" : statusAndMsgs.MSG_MISSING_UNAME_PWD});
         return;
     }
-    const token = generateAccessToken(req.body);
-    res.json({"accessToken" : token});
+    if (_.isEqual(new User(userName, password), admin)){
+        const token = generateAccessToken(req.body);
+        res.json({"accessToken" : token});
+        return;
+    }
+    res.status(statusAndMsgs.STATUS_UNAUTHORIZED).json({"message" : statusAndMsgs.MSG_UNAUTHORIZED});
 }
 
 module.exports = {
