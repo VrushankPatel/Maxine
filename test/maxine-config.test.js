4dec5cc7cb86687350fd542f7a6f4a56188dd211
Vrushank Patel <vrushankpatel5@gmail.com>
2022-04-12 12:16:56 +0530

    Test enhancement

--- test/maxine-config.test.js
+++ test/maxine-config.test.js
@@ -5,6 +5,7 @@ const app = require('..');
 const config = require('../src/config/config');
 const { generateAccessToken } = require('../src/security/jwt');
 const { constants } = require('../src/util/constants/constants');
+const { sssUtil } = require('../src/util/util');
 const { testUser } = require('./testUtil/test-constants');
 var should = chai.should();
 chai.use(require('chai-json'));
@@ -12,13 +13,6 @@ chai.use(chaiHttp);
 
 const fileName = require('path').basename(__filename).replace(".js","");
 const accessToken = generateAccessToken(testUser);
-
-const serviceDataToUpdate = {
-    "logAsync" : true,
-    "heartBeatTimeout" : 12,
-    "logJsonPrettify" : true,
-    "serverSelectionStrategy" : "RH"
-}
 
 describe(`${fileName} : API /api/maxine`, () => {
 
@@ -34,7 +28,9 @@ describe(`${fileName} : API /api/maxine`, () => {
             });
     });
 
-    it('PUT /config -> 200 & should update the config with given data and returns the update status', (done) => {
+    it('PUT /config (Update logAsync) -> 200 & Update config logAsync', (done) => {
+        const serviceDataToUpdate = {"logAsync" : true}
+
         chai.request(app)
             .put('/api/maxine/config')
             .set("Authorization", `Bearer ${accessToken}`)
@@ -46,32 +42,110 @@ describe(`${fileName} : API /api/maxine`, () => {
                 const body = res.body;
                 body.should.be.a('object');
                 body.should.have.own.property("logAsync", "Success");
-                body.should.have.own.property("heartBeatTimeout", "Success");
-                body.should.have.own.property("logJsonPrettify", "Success");
+                config.logAsync.should.be.eql(serviceDataToUpdate["logAsync"]);
                 done();
             });
     });
 
-    it('GET /config -> 200 & we\'ll compare the updated config.', (done) => {
+    it('PUT /config (Update heartBeatTimeout) -> 200 & Update config heartBeatTimeout', (done) => {
+        const serviceDataToUpdate = {"heartBeatTimeout" : 12}
+
         chai.request(app)
-            .get('/api/maxine/config')
+            .put('/api/maxine/config')
             .set("Authorization", `Bearer ${accessToken}`)
+            .set('Content-Type', 'application/json')
+            .send(serviceDataToUpdate)
             .end((_, res) => {
                 res.should.have.status(200);
                 res.should.be.json;
-
                 const body = res.body;
-
-                body.should.be.eql(config);
-
-                body.should.have.own.property("logAsync", serviceDataToUpdate["logAsync"]);
-                body.should.have.own.property("heartBeatTimeout", serviceDataToUpdate["heartBeatTimeout"]);
-                body.should.have.own.property("logJsonPrettify", serviceDataToUpdate["logJsonPrettify"]);
-                body.should.have.own.property("serverSelectionStrategy");
-                const serverSelStratRetrieved = body["serverSelectionStrategy"];
-                const serverSelStratOriginal = constants.SSS[serviceDataToUpdate["serverSelectionStrategy"]];
-                assert.deepEqual(serverSelStratRetrieved, serverSelStratOriginal, "[message]");
+                body.should.be.a('object');
+                body.should.have.own.property("heartBeatTimeout", "Success");
+                config.heartBeatTimeout.should.be.eql(serviceDataToUpdate["heartBeatTimeout"]);
                 done();
             });
     });
+
+    it('PUT /config (Update logJsonPrettify) -> 200 & Update config logJsonPrettify', (done) => {
+        const serviceDataToUpdate = {"logJsonPrettify" : true}
+
+        chai.request(app)
+            .put('/api/maxine/config')
+            .set("Authorization", `Bearer ${accessToken}`)
+            .set('Content-Type', 'application/json')
+            .send(serviceDataToUpdate)
+            .end((_, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                const body = res.body;
+                body.should.be.a('object');
+                body.should.have.own.property("logJsonPrettify", "Success");
+                config.logJsonPrettify.should.be.eql(serviceDataToUpdate["logJsonPrettify"]);
+                done();
+            });
+    });
+
+    it('PUT /config (Update serverSelectionStrategy to RH) -> 200 & Update config serverSelectionStrategy (RH)', (done) => {
+        const serviceDataToUpdate = {"serverSelectionStrategy" : 'RH'}
+
+        chai.request(app)
+            .put('/api/maxine/config')
+            .set("Authorization", `Bearer ${accessToken}`)
+            .set('Content-Type', 'application/json')
+            .send(serviceDataToUpdate)
+            .end((_, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                const body = res.body;
+                body.should.be.a('object');
+                body.should.have.own.property("serverSelectionStrategy", "Success");
+                sssUtil.isRoundRobin().should.be.false;
+                sssUtil.isRendezvousHashing().should.be.true; // We assigned SSS to RH
+                sssUtil.isConsistentHashing().should.be.false;
+                done();
+            });
+    });
+
+    it('PUT /config (Update serverSelectionStrategy to CH) -> 200 & Update config serverSelectionStrategy (CH)', (done) => {
+        const serviceDataToUpdate = {"serverSelectionStrategy" : 'CH'}
+
+        chai.request(app)
+            .put('/api/maxine/config')
+            .set("Authorization", `Bearer ${accessToken}`)
+            .set('Content-Type', 'application/json')
+            .send(serviceDataToUpdate)
+            .end((_, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                const body = res.body;
+                body.should.be.a('object');
+                body.should.have.own.property("serverSelectionStrategy", "Success");
+                sssUtil.isRoundRobin().should.be.false;
+                sssUtil.isRendezvousHashing().should.be.false;
+                sssUtil.isConsistentHashing().should.be.true; // We assigned SSS to CH
+                done();
+            });
+    });
+
+    it('PUT /config (Update serverSelectionStrategy to RR) -> 200 & Update config serverSelectionStrategy (RR)', (done) => {
+        const serviceDataToUpdate = {"serverSelectionStrategy" : 'RR'}
+
+        chai.request(app)
+            .put('/api/maxine/config')
+            .set("Authorization", `Bearer ${accessToken}`)
+            .set('Content-Type', 'application/json')
+            .send(serviceDataToUpdate)
+            .end((_, res) => {
+                res.should.have.status(200);
+                res.should.be.json;
+                const body = res.body;
+                body.should.be.a('object');
+                body.should.have.own.property("serverSelectionStrategy", "Success");
+                sssUtil.isRoundRobin().should.be.true; // We assigned SSS to RR
+                sssUtil.isRendezvousHashing().should.be.false;
+                sssUtil.isConsistentHashing().should.be.false;
+                done();
+            });
+    });
+
 });
