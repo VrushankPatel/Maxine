58894384c4e8a100af12684516a7586ffe51abdf
Vrushank Patel <vrushankpatel5@gmail.com>
2022-04-14 13:11:27 +0530

    added JSDoc

--- src/service/discovery-services/rendezvous-hash-discovery.js
+++ src/service/discovery-services/rendezvous-hash-discovery.js
@@ -5,11 +5,24 @@ const { constants } = require("../../util/constants/constants");
 const separator = Buffer.from('\0');
 
 class RendezvousHashDiscovery{
+    /**
+     * Calls Select method and returns node retrieved from select method.
+     * @param {string} serviceName
+     * @param {string} ip
+     * @returns {object} returns the node by calling select method
+     */
     getNode = (serviceName, ip) => {
         const serviceNodesObj = serviceRegistry.getNodes(serviceName);
-        return this.selectNode(serviceNodesObj, ip) || {};
+        const node = this.selectNode(serviceNodesObj, ip) || {};
+        return node;
     }
 
+    /**
+     * Selects the node based on hash-digest of IP.
+     * @param {object} nodes
+     * @param {string} ip
+     * @returns {object} select the node based on IP Hashing
+     */
     selectNode(nodes, ip) {
         if(_.isUndefined(nodes)) return {};
         let targetNodeId, targetNodeRank = 0;
@@ -23,6 +36,12 @@ class RendezvousHashDiscovery{
         return nodes[targetNodeId];
     }
 
+    /**
+     * Returns rank by hashing nodeId and ip with hash algo defined in constants.
+     * @param {string} nodeId
+     * @param {string} ip
+     * @returns {number} return rank by hashing nodeId and ip with hash algo defined in constants.
+     */
     rank = (nodeId, ip)  => crypto
                                 .createHash(constants.RENDEZVOUS_HASH_ALGO)
                                 .update(nodeId)
