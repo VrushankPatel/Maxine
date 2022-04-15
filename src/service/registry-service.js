d62a1fabe836dfc3b04e6d6a08cbba318d6114cf
Vrushank Patel <vrushankpatel5@gmail.com>
2022-04-15 07:42:20 +0530

    refactoring

--- src/service/registry-service.js
+++ src/service/registry-service.js
@@ -1,27 +1,27 @@
 const _ = require('lodash');
-const { serviceRegistry } = require("../entity/service-registry");
+const { serviceRegistry: s_Registry } = require("../entity/service-registry");
 class RegistryService{
 
     registerService = (serviceObj) => {
         const {serviceName, nodeName, address, timeOut, weight} = serviceObj;
 
-        if (!serviceRegistry.registry[serviceName]){
-            serviceRegistry.registry[serviceName] = {offset: 0, nodes: {}};
+        if (!s_Registry.registry[serviceName]){
+            s_Registry.registry[serviceName] = {offset: 0, nodes: {}};
         }
 
         [...Array(weight).keys()].forEach(index => {
             const subNodeName = `${nodeName}-${index}`;
 
-            serviceRegistry.addNodeToHashRegistry(serviceName, subNodeName);
+            s_Registry.addNodeToHashRegistry(serviceName, subNodeName);
 
-            if(serviceRegistry.registry[serviceName]["nodes"][subNodeName]){
+            if(s_Registry.registry[serviceName]["nodes"][subNodeName]){
                 clearTimeout(
-                    serviceRegistry
-                        .timeResetters[serviceRegistry
+                    s_Registry
+                        .timeResetters[s_Registry
                                         .registry[serviceName]["nodes"][subNodeName]["nodeName"]]);
             }
 
-            serviceRegistry.registry[serviceName]["nodes"][subNodeName] = {
+            s_Registry.registry[serviceName]["nodes"][subNodeName] = {
                 "nodeName" : subNodeName,
                 "parentNode" : nodeName,
                 "address" : address,
@@ -30,17 +30,17 @@ class RegistryService{
             }
 
             const timeResetter = setTimeout(() => {
-                delete serviceRegistry.registry[serviceName]["nodes"][subNodeName];
-                if(Object.keys(serviceRegistry.registry[serviceName]["nodes"]).length === 0){
-                    delete serviceRegistry.registry[serviceName];
+                delete s_Registry.registry[serviceName]["nodes"][subNodeName];
+                if(Object.keys(s_Registry.registry[serviceName]["nodes"]).length === 0){
+                    delete s_Registry.registry[serviceName];
                 }
-                serviceRegistry.removeNodeFromRegistry(serviceName, subNodeName);
-                if(Object.keys(serviceRegistry.hashRegistry[serviceName]["nodes"]).length === 0){
-                    delete serviceRegistry.hashRegistry[serviceName];
+                s_Registry.removeNodeFromRegistry(serviceName, subNodeName);
+                if(Object.keys(s_Registry.hashRegistry[serviceName]["nodes"]).length === 0){
+                    delete s_Registry.hashRegistry[serviceName];
                 }
             }, ((timeOut)*1000)+500);
 
-            serviceRegistry.timeResetters[subNodeName] = timeResetter;
+            s_Registry.timeResetters[subNodeName] = timeResetter;
         });
     }
 
