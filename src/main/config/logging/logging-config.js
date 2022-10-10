82eaa50723d5b23d214f4cc354a1e215dc469392
Vrushank Patel <vrushankpatel5@gmail.com>
2022-10-10 09:55:45 +0530

    disabled file logging

--- src/main/config/logging/logging-config.js
+++ src/main/config/logging/logging-config.js
@@ -3,14 +3,14 @@ const { format } = require('winston');
 const { constants } = require('../../util/constants/constants');
 
 const logFileTransports = [new winston.transports.Console()]
-    .concat(constants.LOGLEVELS.map(logLevel => new winston.transports.File({
-        level: logLevel,
-        filename: `logs/Maxine-${logLevel}.log`,
-        maxsize:1000000,
-        keep: 5,
-        compress: true
-    }))
-);
+    // .concat(constants.LOGLEVELS.map(logLevel => new winston.transports.File({
+    //     level: logLevel,
+    //     filename: `logs/Maxine-${logLevel}.log`,
+    //     maxsize:1000000,
+    //     keep: 5,
+    //     compress: true
+    // }))
+// );
 
 let last100LogsTrack = [];
 
